b9f9abff7d9d67e80f5e4bc16300e49e1d9cddc3
bmalkmus <bmalkmus23@gmail.com>
2020-09-24 11:08:59 -0700

    production build

---
+++ client/build/precache-manifest.eb53a4d23092fe982c762497889ead4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e247075cb703e8ddae",
+    "url": "/static/css/main.180c72a8.chunk.css"
+  },
+  {
+    "revision": "52e247075cb703e8ddae",
+    "url": "/static/js/main.327070ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed1332b57980a9f34a93",
+    "url": "/static/js/2.6c28f777.chunk.js"
+  },
+  {
+    "revision": "edf4a14593d5d2d9d7e48989b8e2e883",
+    "url": "/index.html"
+  }
+];
