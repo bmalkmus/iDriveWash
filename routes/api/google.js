d69a95529e757d8700d47b4eb5ac7a3d0c65d360
bmalkmus <bmalkmus23@gmail.com>
2020-10-03 21:42:59 -0700

    backend connected

--- routes/api/google.js
+++ routes/api/google.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
+const axios = require ('axios')
 
-router.route("/")
+const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API}`
+
+// const getData = async (url) => {
+//     try {
+//       const response = axios.get(url)
+//       const data = response.data
+//       return data
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+router.get("/", function (req, response) {
+    
+    axios.get(url)
+    .then(res => {
+        response.send(res.data)
+    })
+})
 
 
 module.exports = router;
